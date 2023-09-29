87e32a7992a669cf2b28535f3a3ca8bb1b401ee1
Anton Khoroshok <tohik05@gmail.com>
2023-09-29 16:10:41 +0300

    fixed report path

--- src/main/java/com/andersen/mail/EmailDailySender.java
+++ src/main/java/com/andersen/mail/EmailDailySender.java
@@ -8,11 +8,6 @@ import java.time.LocalDate;
 
 public class EmailDailySender {
 
-    public static void main(String[] args) {
-        EmailDailySender sender = new EmailDailySender();
-        sender.sendEmail();
-    }
-
     private static ReportService reportService = ReportServiceImpl.getInstance();
     private static final PropertiesLoader props = new PropertiesLoader();
     private final String from = props.getProperty("mail.from");
@@ -24,7 +19,7 @@ public class EmailDailySender {
     public void sendEmail() {
         String subject = "Daily report";
         String body = "It's a today daily report: " + LocalDate.now();
-        String reportFile = "src\\main\\resources\\reports\\DailyReport.pdf";
+        String reportFile = props.getProperty("reportPath");
         String report = reportService.createDailyReport(LocalDate.now(), reportFile);
         MailWithAttachmentService mail = new MailWithAttachmentService(from, password, host, port, to);
         mail.sendMail(subject, body, report);
