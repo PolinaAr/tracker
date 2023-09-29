7935e72a44dcdf4db8d7f8c157e32510b1e051f3
Polina Arkhipenko <polinar977@gmail.com>
2023-09-29 16:51:40 +0300

    added listener with scheduler

--- src/main/java/com/andersen/scheduler/EmailDailySenderJob.java
+++ src/main/java/com/andersen/scheduler/EmailDailySenderJob.java
@@ -1,5 +1,6 @@
-package com.andersen.mail;
+package com.andersen.scheduler;
 
+import com.andersen.mail.MailWithAttachmentService;
 import com.andersen.report.ReportService;
 import com.andersen.report.ReportServiceImpl;
 import com.andersen.util.PropertiesLoader;
@@ -18,13 +19,12 @@ public class EmailDailySenderJob implements Job {
     private final String host = props.getProperty("mail.host");
     private final int port = Integer.parseInt(props.getProperty("mail.port"));
     private final String to = props.getProperty("mail.to");
+    private final String reportFile = props.getProperty("reportPath");
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("\n\n\n------sending email-----------\n\n\n");
         String subject = "Daily report";
         String body = "It's a today daily report: " + LocalDate.now();
-        String reportFile = props.getProperty("reportPath");
         String report = reportService.createDailyReport(LocalDate.now(), reportFile);
         MailWithAttachmentService mail = new MailWithAttachmentService(from, password, host, port, to);
         mail.sendMail(subject, body, report);
