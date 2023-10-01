18cf946ca545eb51c4d1006fab81151185090f9f
Anton Khoroshok <tohik05@gmail.com>
2023-10-01 21:08:07 +0300

    - updated cors rules

--- src/main/java/com/andersen/config/CORSFilter.java
+++ src/main/java/com/andersen/config/CORSFilter.java
@@ -14,28 +14,11 @@ import java.io.IOException;
 @WebFilter("/*")
 public class CORSFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest request = (HttpServletRequest) servletRequest;
-//
-//        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-//        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS,HEAD");
-//        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers","Content-Type, API-Key");
-//        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
-//
-//        filterChain.doFilter(request, servletResponse);
-//    }
-
-    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-//        httpResponse.setHeader("Access-Control-Allow-Origin", "http://34.118.36.24:3000");
-        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://34.118.36.24:3000");
+//        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
@@ -46,6 +29,11 @@ public class CORSFilter implements Filter {
         httpResponse.setHeader("Access-Control-Max-Age", "3600");
 
         chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
     }
 
     @Override
