831a6a20d059f914cf3834ef101183ce62a2d4fb
Anton Khoroshok <tohik05@gmail.com>
2023-10-01 18:32:59 +0300

    - updated cors rules

--- src/main/java/com/andersen/config/CORSFilter.java
+++ src/main/java/com/andersen/config/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "http://34.118.36.24:3000/");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS,HEAD");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-HEADERS","Content-Type, API-Key");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
